2c011d622c50233bb5f496436c75881532a7e843
gabsartori <130071224+gabsartori@users.noreply.github.com>
2023-10-15 21:46:28 -0300

    Add files via upload

---
+++ sistemaLivros/src/test/kotlin/br/upf/sistemaLivros/SistemaLivrosApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.upf.sistemaLivros
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SistemaLivrosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
